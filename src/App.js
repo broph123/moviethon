f4e2f8153ceb39f29da21e2afaa32333c9231dba
Henry Brophy <henrybrophy@henrys-mbp.lan>
2021-04-29 09:25:53 -0400

    API key is working.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
 
+
 function App() {
+  
+
+  const getGodfather = ()=>{
+    axios
+    .get(`http://www.omdbapi.com/?t=The+Godfather&apikey=${process.env.REACT_APP_API_KEY}`)
+    .then(res =>console.log(res.data))
+    .catch(err=>console.log(err.response))
+  }
+
+  useEffect(()=>{
+    getGodfather();
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Shoppies
+        <div></div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// Component that searches through the movie DB
+// Shows the Movie
+// Shows which movies have been nominated.
+
